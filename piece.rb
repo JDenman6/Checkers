134ed963ce35e0b0be65ed51cf5d67407f12bd91
JDenman6 <JDenman6@gmail.com>
2015-08-06 11:01:44 -0400

    Redefine Piece#to_S for better display

--- piece.rb
+++ piece.rb
@@ -1,11 +1,21 @@
 class Piece
-  attr_reader :color
-  attr_accessor :pos
+
+  attr_accessor :pos, :color
 
   def initialize(pos, color)
     @color = color
     @pos = pos
   end
 
+  def to_s
+    "#{self.color}"
+  end
 
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  red = Piece.new([0,0], :r)
+
+  p red.to_s
 end
